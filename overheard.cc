5f4ee6059ac7f06fe6918e279eb2a225ff765984
Lucas Thoresen <reoky1@gmail.com>
2013-11-03 23:51:24 -0500

    Update #1

--- overheard.cc
+++ overheard.cc
@@ -5,11 +5,11 @@
 
 using namespace std;
 
+
 int main(int argc, char ** argv)
 { 
-    //thread t1(task1, "Hello");
-    // Makes the main thread wait for the new thread to finish execution, therefore blocks execution.
-    //t1.join();
+    //thread coreThread(testTask, "");
+    //coreThread.join();
     cout << "Currently Programming" << endl;
     return 0;
 }
